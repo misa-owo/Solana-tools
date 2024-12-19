4563d36712c70556e569a4cc99595b591b8651a2
SulemanRasheed132446 <sulemanrasheed132446@gmail.com>
2024-12-20 03:41:52 +0500

    wip: add the responsiveness for guides page

--- src/app/guides/[slug]/page.tsx
+++ src/app/guides/[slug]/page.tsx
@@ -19,25 +19,25 @@ export default async function Category({ params }: Props) {
         <div className="w-full  text-white text-center text-sm flex flex-col items-center relative">
             <p className="mt-[10%] bg-[#ffffff14] w-fit m-auto px-6 text-xs font-semibold  py-1  rounded-xl  border-[2px] border-[#ffffff14] shadow-xl">Support Us and Help Us Grow! Your Backing Means Everything! 🤩</p>
             <Image src={project.logo} width={100} height={100} alt={project.name} className=" mt-10 rounded-[50%]" />
-            <h1 className="mt-10 text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]"> {guide.title}</h1>
-            <p className="text-[#abadaf] text-xl mt-10  w-8/12">{guide.description} </p>
+            <h1 className="mt-4 md:mt-10 text-2xl w-full md:text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-9/12 md:w-[50%]"> {guide.title}</h1>
+            <p className="text-[#abadaf]  text-sm md:text-xl mt-2 md:mt-10  w-full md:w-8/12">{guide.description} </p>
         </div>
         <div className="w-full  text-white text-center text-sm flex flex-col items-center my-[5%]">
-            <h1 className="mt-10 text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]"> {guide.question.title}</h1>
-            <p className="text-[#abadaf] text-xl mt-10  w-8/12">{guide.question.paragraph} </p>
+            <h1 className="mt-5 md:mt-10 text-2xl md:text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-full md:w-[50%]"> {guide.question.title}</h1>
+            <p className="text-[#abadaf] text-sm md:text-xl mt-2 md:mt-10 w-full md:w-8/12">{guide.question.paragraph} </p>
         </div>
         {guide.steps.map((step, index) =>
-            <div key={step.details.title} className="flex justify-between mb-[5%] items-center" style={{ flexDirection: index % 2 === 0 ? "row-reverse" : "row" }}>
-                <div className="w-6/12">
-                    <h1 className="mt-10 mb-2 text-3xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] "> Step {index + 1}: {step.details.title}</h1>
+            <div key={step.details.title} className="flex justify-between mb-[5%] items-center flex-wrap" style={{ flexDirection: index % 2 === 0 ? "row-reverse" : "row" }}>
+                <div className="w-full md:w-6/12 mb-10 md:mb-0">
+                    <h1 className="mt-10 mb-2 text-md md:text-3xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] "> Step {index + 1}: {step.details.title}</h1>
                     {step.details.paragraph.map(paragraph =>
-                        <p className="text-[#abadaf] text-xl mt-2" key={paragraph}>
+                        <p className="text-[#abadaf] text-xs md:text-xl mt-2" key={paragraph}>
                             {paragraph}
                         </p>
                     )}
                 </div>
-                <div className="w-5/12">
-                    <div className="shadow-xl shadow-[#8952e0]/10 flex flex-col justify-center items-center w-full">
+                <div className="w-full md:w-5/12">
+                    <div className="shadow-xl shadow-[#8952e0]/10 flex flex-col justify-center items-center w-full ">
                         <Image
                             alt="Mountains"
                             layout="responsive"
@@ -55,8 +55,8 @@ export default async function Category({ params }: Props) {
             </div>)
         }
         <div className="w-full  text-white text-center text-sm flex flex-col items-center my-[5%]">
-            <h1 className="mt-10 text-4xl font-bold w-[50%]"> ❓</h1>
-            <h1 className="mt-5 mb-10 text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]">{project.name} FAQ</h1>
+            <h1 className="mt-10 text-2xl md:text-4xl font-bold w-[50%]"> ❓</h1>
+            <h1 className="mt-5 md:mb-10 text-2xl md:text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]">{project.name} FAQ</h1>
             {guide.faqs.map(guide => <FAQ {...guide} key={guide.title}/>)}
         </div>
     </div >
--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -9,13 +9,13 @@ interface QuestionProps {
 
 const FAQ: React.FC<QuestionProps> = ({ title, paragraph }) => {
     const [open, setOpen] = useState(false)
-    return (<div key={title} className=" shadow-xl shadow-[#8952e0]/10 mt-5 block relative text-left text-white w-full  bg-[linear-gradient(to_bottom_left,_rgba(255,_255,_255,_0.04),_transparent)] border-[#00000014] rounded-xl p-8 shadow-xl">
+    return (<div key={title} className=" shadow-xl shadow-[#8952e0]/10 mt-5 block relative text-left text-white w-full  bg-[linear-gradient(to_bottom_left,_rgba(255,_255,_255,_0.04),_transparent)] border-[#00000014] rounded-xl p-4 md:p-8 shadow-xl">
         <div className="flex justify-between">
-            <p className="text-2xl font-bold">{title}</p>
+            <p className="text-md md:text-2xl font-bold">{title}</p>
             {!open ? <ChevronDown onClick={() => setOpen(!open)} className="cursor-pointer" /> :
                 <ChevronUp onClick={() => setOpen(!open)} className="cursor-pointer" />}
         </div>
-        <p className={`text-[#abadaf] text-xl mt-3 w-8/12 transition:all h-0 overflow-hidden  ${open && "h-full"}`}>{paragraph} </p>
+        <p className={`text-[#abadaf] text-xs md:text-xl mt-3 md:w-8/12 transition:all h-0 overflow-hidden  ${open && "h-full"}`}>{paragraph} </p>
     </div>)
 }
 export default FAQ;
