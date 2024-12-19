c7914198d771402c54e903bb3969f44a455ab965
SulemanRasheed132446 <sulemanrasheed132446@gmail.com>
2024-12-20 04:37:03 +0500

    wip: add the tool responsiveness

--- src/app/tools/[slug]/page.tsx
+++ src/app/tools/[slug]/page.tsx
@@ -16,12 +16,12 @@ export default async function Category({ params }: Props) {
     const category = categories.find(c => c.projects.find(project => project.slug === slug));
     const project = category?.projects.find(project => project.slug === slug);
     return <div className='px-[10%]'>
-        <div className="w-full  text-white text-center text-sm flex flex-col items-center">
+        <div className="w-full  text-white text-center text-sm flex flex-col items-center md:mb-0 mb-10">
             <p className="mt-[10%] bg-[#ffffff14] w-fit m-auto px-6 text-xs font-semibold  py-1  rounded-xl  border-[2px] border-[#ffffff14] shadow-xl">Support Us and Help Us Grow! Your Backing Means Everything! 🤩</p>
-            <h1 className="mt-10 text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]"> {project?.name}</h1>
+            <h1 className="mt-4 md:mt-10 text-3xl md:text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]"> {project?.name}</h1>
             <div className="flex ml-4">
-                <h1 className="mt-2 text-xl text-white  bg-[#ffffff14] cursor-pointer w-fit m-auto px-2 text-xs font-semibold  py-1 rounded-xl border-2  mr-2 "> {category?.title}</h1>
-                <h1 className="mt-2 text-xl text-white bg-[#ffffff14] cursor-pointer w-fit m-auto px-2 text-xs font-semibold  py-1 rounded-xl border-2  "> {project?.paid ? "FREE" : "PAID"} </h1>
+                <h1 className="mt-2 md:text-xl text-white  bg-[#ffffff14] cursor-pointer w-fit m-auto px-2 text-xs font-semibold  py-1 rounded-xl border-2  mr-2 "> {category?.title}</h1>
+                <h1 className="mt-2 md:text-xl text-white bg-[#ffffff14] cursor-pointer w-fit m-auto px-2 text-xs font-semibold  py-1 rounded-xl border-2  "> {project?.paid ? "FREE" : "PAID"} </h1>
             </div>
         </div>
         <div className="flex flex-wrap gap-4 md:gap-10 mt-[5%] md:mt-[10%]">
@@ -67,11 +67,11 @@ const DetailComponent: React.FC<DetailsComponent> = (component) => {
 
 const TrialLinkComponent: React.FC<TrialLink> = (component) => {
     return (
-        <div className=" shadow-xl shadow-[#8952e0]/10 mt-5 block flex relative text-white w-full flex items-center  justify-between bg-[linear-gradient(to_bottom_left,_rgba(255,_255,_255,_0.04),_transparent)] border-[#00000014] rounded-xl p-12 shadow-xl">
+        <div className=" shadow-xl shadow-[#8952e0]/10 mt-5 block flex flex-col md:flex-row relative text-white w-full flex items-center  justify-between bg-[linear-gradient(to_bottom_left,_rgba(255,_255,_255,_0.04),_transparent)] border-[#00000014] rounded-xl p-6 md:p-12 shadow-xl">
             <div>
-                <p className="text-4xl font-bold">{component.title}</p>
+                <p className="text-xl md:text-4xl font-bold">{component.title}</p>
             </div>
-            <Button className="rounded-xl text-white shadow-xl ml-20 bg-[#8952e0]" size="lg">{component.buttonText}</Button>
+            <Button className="rounded-xl text-white mt-4 md:mt-0 shadow-xl md:ml-20 w-full md:w-fit bg-[#8952e0]" size="lg">{component.buttonText}</Button>
         </div>)
 }
 
@@ -97,7 +97,7 @@ const ImageComponent: React.FC<ImageHeader> = ({ img_url }) => {
 
 const DetailListComponent: React.FC<DetailsListComponent> = (component) => {
     return (
-        <div className="shadow-xl text-white shadow-[#8952e0]/10 w-full mt-[2%] bg-[linear-gradient(to_bottom_left,_rgba(255,_255,_255,_0.04),_transparent)] border-[#00000014] rounded-xl p-12 shadow-xl">
+        <div className="shadow-xl text-white shadow-[#8952e0]/10 w-full mt-[2%] bg-[linear-gradient(to_bottom_left,_rgba(255,_255,_255,_0.04),_transparent)] border-[#00000014] rounded-xl p-6 md:p-12 shadow-xl">
             <p className="font-bold text-xl md:text-3xl mb-2">{component.title}</p>
             <ul className="list-disc ml-5 mt-4">
                 {component.list.map((paragraph) => (
