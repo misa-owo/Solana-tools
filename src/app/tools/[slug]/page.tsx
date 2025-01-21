e7bc1c5a4d9b23692ab1e50427bb2566f1195f4d
Misa <misaowoemail@gmail.com>
2025-01-21 01:21:06 -0500

    updated website with new ref links, and added new tg button

--- src/app/tools/[slug]/page.tsx
+++ src/app/tools/[slug]/page.tsx
@@ -3,6 +3,7 @@ import categories from "../../../../data/categories"
 import Contact from "@/components/contact";
 import { Metadata } from "next";
 import Link from "next/link";
+import { Send } from "lucide-react";
 
 
 interface Props {
@@ -36,14 +37,24 @@ export default async function Category({ params }: Props) {
         <div className="w-full  text-white text-center text-sm flex flex-col items-center md:mb-0 mb-5 mt-[8%]">
           <h1 className="mt-4 md:mt-10 text-3xl md:text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]"> {project?.name}</h1>
             <div className="flex ml-4">
-                <h1 className="mt-2 md:text-xl text-white  bg-[#ffffff14] cursor-pointer w-fit m-auto px-2 text-xs font-semibold  py-1 rounded-xl border-2  mr-2 "> {category?.title}</h1>
-                <h1 className="mt-2 md:text-xl text-white bg-[#ffffff14] cursor-pointer w-fit m-auto px-2 text-xs font-semibold  py-1 rounded-xl border-2  "> {project?.paid ? "FREE" : "PAID"} </h1>
+                <Link href={project?.link || "/"}><h1 className="mt-2 md:text-m text-white bg-[#ffffff14] cursor-pointer w-fit m-auto text-xs   "> {project?.summary} </h1></Link>
             </div>
-            <p className="mt-[3%] bg-[#ffffff14] w-fit m-auto px-6 text-xs font-semibold  py-1  rounded-xl  border-[2px] border-[#ffffff14] shadow-xl"><Link href={project?.link || "/"}>✅ Tap here to register for {project?.name}</Link></p>
+            <p className="flex items-center mt-[3%] bg-[#26513a] hover:bg-[#26513a] text-white border-[#459c6e] border-[1px] rounded-xl px-6 py-1 shadow-xl">
+  <Send className="mr-2 text-white" />
+  <Link href={project?.link || "/"} className="text-white">Join {project?.name} with Telegram!</Link>
+</p>
+
 
         </div>
         <div className="content flex flex-wrap gap-4 md:gap-10 mt-[5%] md:mt-[5%] justify-center text-white" dangerouslySetInnerHTML={{ __html: project?.content || "" }}>
         </div>
+        <div className="w-full  text-white text-center text-sm flex flex-col items-center md:mb-0 mb-5 mt-[8%]">
+        <p className="flex items-center mt-[3%] bg-[#26513a] hover:bg-[#26513a] text-white border-[#459c6e] border-[1px] rounded-xl px-6 py-1 shadow-xl">
+  <Send className="mr-2 text-white" />
+  <Link href={project?.link || "/"} className="text-white">Join {project?.name} with Telegram!</Link>
+</p>
+</div>
+
         <Contact />
     </div>
 }
