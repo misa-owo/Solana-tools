94f5a7da00dc5f89c35a1adc177088a2855fc39f
Misa <misaowoemail@gmail.com>
2025-01-21 01:23:06 -0500

    added _blank to tool links

--- src/app/tools/[slug]/page.tsx
+++ src/app/tools/[slug]/page.tsx
@@ -37,11 +37,11 @@ export default async function Category({ params }: Props) {
         <div className="w-full  text-white text-center text-sm flex flex-col items-center md:mb-0 mb-5 mt-[8%]">
           <h1 className="mt-4 md:mt-10 text-3xl md:text-6xl text-transparent bg-clip-text font-bold bg-[linear-gradient(to_right_bottom,_rgb(255,255,255)_30%,_rgba(255,255,255,0.6))] w-[50%]"> {project?.name}</h1>
             <div className="flex ml-4">
-                <Link href={project?.link || "/"}><h1 className="mt-2 md:text-m text-white bg-[#ffffff14] cursor-pointer w-fit m-auto text-xs   "> {project?.summary} </h1></Link>
+                <Link href={project?.link || "/"} target="_blank"><h1 className="mt-2 md:text-m text-white bg-[#ffffff14] cursor-pointer w-fit m-auto text-xs   "> {project?.summary} </h1></Link>
             </div>
             <p className="flex items-center mt-[3%] bg-[#26513a] hover:bg-[#26513a] text-white border-[#459c6e] border-[1px] rounded-xl px-6 py-1 shadow-xl">
   <Send className="mr-2 text-white" />
-  <Link href={project?.link || "/"} className="text-white">Join {project?.name} with Telegram!</Link>
+  <Link href={project?.link || "/"} target="_blank" className="text-white">Join {project?.name} with Telegram!</Link>
 </p>
 
 
@@ -51,7 +51,7 @@ export default async function Category({ params }: Props) {
         <div className="w-full  text-white text-center text-sm flex flex-col items-center md:mb-0 mb-5 mt-[8%]">
         <p className="flex items-center mt-[3%] bg-[#26513a] hover:bg-[#26513a] text-white border-[#459c6e] border-[1px] rounded-xl px-6 py-1 shadow-xl">
   <Send className="mr-2 text-white" />
-  <Link href={project?.link || "/"} className="text-white">Join {project?.name} with Telegram!</Link>
+  <Link href={project?.link || "/"} target="_blank" className="text-white">Join {project?.name} with Telegram!</Link>
 </p>
 </div>
 
